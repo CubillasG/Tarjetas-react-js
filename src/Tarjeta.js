482de0929ec9c225b98234f9aa1a969e3b76aee4
Gonzalo Cubillas <gonzalo.cubillas15@gmail.com>
2021-08-25 18:09:20 -0300

    segunda parte

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import imagen1 from './img/html.png'
+import React from 'react';
 
-function Tarjeta() {
+
+function Tarjeta({titulo,misImagenes}) {
     return (
-        <div className='card'>
-            <img src={imagen1}/>
+        <div className='card bg-dark'>
+            <img src={misImagenes}/>
             <div className='card-body'>
-                <h4 className='card-title'>Soy Gonzalo</h4>
+                <h4 className='card-title'>{titulo}</h4>
                 <p className='card-text text-secondary'> Mi primer experiencia con la programacion fue mediante HTML 5 y CSS </p>
-                    
+                <button type="button" className="btn btn-secondary">Ir al sitio web</button>  
             </div>
           
         </div>
--- src/Tarjetas.js
+++ src/Tarjetas.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import Tarjeta from './Tarjeta'
 import './tarjetas.css'
+import imagen1 from './img/html.png'
+import imagen2 from './img/js.png'
+import imagen3 from './img/react.jpg'
+
+const tarjetas = [
+    {
+    id: 1,
+    title: 'html5 y css',
+    image: imagen1
+    },
+    {
+        id: 2,
+        title: 'Javascript',
+        image: imagen2
+    },
+    {
+        id: 3,
+        title: 'React js',
+        image: imagen3
+    }
+
+]
 
 function Tarjetas() {
     return (
         <div className='container'>
             <div className='row'>
-                <div className='col-md-4'>
-                     <Tarjeta/>
-               </div>
+                {
+                    tarjetas.map(tarjeta =>
+                        <div className='col-md-4' key={tarjeta.id}>
+                             <Tarjeta titulo={tarjeta.title} misImagenes={tarjeta.image}/>
+                        </div>  
+                        )
+                }
 
-               <div className='col-md-4'>
-                     <Tarjeta/>
-               </div>
-
-               <div className='col-md-4'>
-                     <Tarjeta/>
-               </div>
+               
               </div>
         </div>
     )
