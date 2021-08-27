b86329d1829a7b7ebe21ed51ae929ac7f56b4e6e
Gonzalo Cubillas <gonzalo.cubillas15@gmail.com>
2021-08-27 11:51:52 -0300

    terminado tarjetas

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 
-function Tarjeta({titulo,misImagenes}) {
+function Tarjeta({titulo,misImagenes,texto,links}) {
     return (
-        <div className='card bg-dark'>
+        <div className='card'>
             <img src={misImagenes}/>
-            <div className='card-body'>
-                <h4 className='card-title'>{titulo}</h4>
-                <p className='card-text text-secondary'> Mi primer experiencia con la programacion fue mediante HTML 5 y CSS </p>
-                <button type="button" className="btn btn-secondary">Ir al sitio web</button>  
+            <div className='card-body todo'>
+                <h4 className='card-title todo'>{titulo}</h4>
+                <p className='card-text'>{texto}</p>
+                <a href={links} target='new'><button type="button" class="btn btn-primary">Ir a Git Hub</button></a>   
             </div>
           
         </div>
     )
+}
+Tarjeta.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    misImagenes: PropTypes.string,
+    links: PropTypes.string.isRequired
 }
 
 export default Tarjeta
--- src/Tarjetas.js
+++ src/Tarjetas.js
@@ -9,17 +9,23 @@ const tarjetas = [
     {
     id: 1,
     title: 'html5 y css',
-    image: imagen1
+    image: imagen1,
+    texto: 'HTML es el lenguaje de marcado a partir del cual se define todo el contenido que vemos en las páginas, y junto a él está el CSS, orientado hacia su diseño y apariencia. De hecho, aunque muchas personas desconocen qué es HTML y CSS, es muy probable que disfruten a diario de sus aplicaciones al navegar por la red.',
+    url: "https://github.com/CubillasG/Modern.Desing"
     },
     {
         id: 2,
         title: 'Javascript',
-        image: imagen2
+        image: imagen2,
+        texto: 'JavaScript es un lenguaje de programación o de secuencias de comandos que te permite implementar funciones complejas en páginas web, cada vez que una página web hace algo más que sentarse allí y mostrar información estática para que la veas, muestra oportunas actualizaciones de contenido, mapas interactivos, animación de Gráficos 2D/3D, desplazamiento de máquinas reproductoras de vídeo, etc.',
+        url: "https://github.com/CubillasG/MyTienda"
     },
     {
         id: 3,
         title: 'React js',
-        image: imagen3
+        image: imagen3,
+        texto: 'React es una biblioteca escrita en JavaScript, desarrollada en Facebook para facilitar la creación de componentes interactivos, reutilizables, para interfaces de usuario. Se utiliza en Facebook para la producción de componentes, e Instagram está escrito enteramente en React. Uno de sus puntos más destacados, es que no sólo se utiliza en el lado del cliente, sino que también se puede representar en el servidor, y trabajar juntos.',
+        url: "https://github.com/CubillasG/Buscador-de-Libros"
     }
 
 ]
@@ -31,7 +37,7 @@ function Tarjetas() {
                 {
                     tarjetas.map(tarjeta =>
                         <div className='col-md-4' key={tarjeta.id}>
-                             <Tarjeta titulo={tarjeta.title} misImagenes={tarjeta.image}/>
+                             <Tarjeta titulo={tarjeta.title} misImagenes={tarjeta.image} texto={tarjeta.texto} links={tarjeta.url}/>
                         </div>  
                         )
                 }
